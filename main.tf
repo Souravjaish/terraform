14086d87c94fa1267b204c3a2c6c6d92fccb0177
Souravjaish <107905959+Souravjaish@users.noreply.github.com>
2023-03-23 11:54:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ terraform {
 resource "aws_instance" "myec2" {
   ami           = "ami-00ae10ea2db12689d"
   instance_type = "t2.micro"
-  key_name      = "mykeypair"
   
   tags = {
     Name = "example-instance"
